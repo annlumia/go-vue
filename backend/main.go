cec6ad8a8b733a0a469387e6bf16109671fde586
annlumia <ann.lumia@live.com>
2021-07-10 11:22:42 +0700

    first commit

---
+++ backend/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+	e.Static("/static", "static")
+
+	e.GET("/user", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, map[string]string{"name": "Jhon Doe"})
+	})
+
+	e.Start(":3000")
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,28 @@
+<template>
+  <div style="margin-top:20px">
+    <h3>
+      {{ judulHalaman }}
+    </h3>
+
+    <form>
+      <input type="text" name="" id="" placeholder="User/email" />
+      <input type="password" name="" id="" placeholder="password" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      judulHalaman: 'Login',
+    }
+  },
+
+  methods: {
+    getJudul() {
+      this.judulHalaman = 'JUDUL BARU'
+    },
+  },
+}
+</script>
